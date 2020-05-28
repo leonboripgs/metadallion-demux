5f3cf013ca70dc76641a4a3d82a4376c1ee8966a
Maxsim <leonboripgs0818@outlook.com>
2020-05-28 16:03:12 +1000

    [url] update blockchain url

--- services/demux/index.js
+++ services/demux/index.js
@@ -7,7 +7,7 @@ const actionHandler = new ActionHandler([handlerVersion]);
 const actionReader = new NodeosActionReader(
   // original v1.3 testnet: http://localhost:7777
   // gitpod v2.0 testnet: "https://8000-d98ffe18-8038-4b14-9af3-5bc105bc441c.ws-us02.gitpod.io",
-  "EOS BLOCKCHAIN HTTP ENDPOINT GOES HERE", // Blockchain
+  "http://localhost:8888", // Blockchain
   0,
 );
 
